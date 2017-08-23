23e99ec99342ee897702a372725fdc1bca4a9faf
Xu Zheng <sam.z.xu@gmail.com>
2017-08-23 16:30:46 +0800

    another way to address inview problem

--- js/adapt-chartjs.js
+++ js/adapt-chartjs.js
@@ -1,113 +1,84 @@
 define(function(require) {
+  var Adapt = require('coreJS/adapt');
+  var ComponentView = require('coreViews/componentView');
+  var Chart = require('./Chart.min');
+  var ChartJs = ComponentView.extend({
+    events: {},
 
-		var Adapt = require('coreJS/adapt');
-		var ComponentView = require('coreViews/componentView');
-		var Chart = require('./Chart.min');
-    var ChartJs = ComponentView.extend({
+    preRender: function() {
+      this.listenTo(Adapt, 'device:resize', this.onScreenSizeChanged);
+      this.listenTo(Adapt, 'device:changed', this.onDeviceChanged);
+      this.listenTo(Adapt, 'accessibility:toggle', this.onAccessibilityToggle);
+      this.checkIfResetOnRevisit();
+    },
 
-        events: {
+    postRender: function() {
+      this.setupChart();
+      this.$('.component-widget').on('inview', _.bind(this.inview, this));
+    },
 
-        },
+    setupChart: function() {
+      var ctx = $('#myChart' + this.model.get('_id'));
+      var myChart = new Chart(ctx, {
+        type: this.model.get('_chartType'),
+        data: this.model.get('data'),
+        options: this.model.get('_options'),
+      });
 
-        preRender: function() {
-            this.listenTo(Adapt, 'device:resize', this.onScreenSizeChanged);
-            this.listenTo(Adapt, 'device:changed', this.onDeviceChanged);
-            this.listenTo(Adapt, 'accessibility:toggle', this.onAccessibilityToggle);
-            this.checkIfResetOnRevisit();
-        },
+      this.setReadyStatus();
+    },
 
-        postRender: function() {
-            this.setupChart();
-            // Check if instruction or title or body is set, otherwise force completion
-            var cssSelector = this.$('.component-instruction').length > 0
-                ? '.component-instruction'
-                : (this.$('.component-title').length > 0
-                ? '.component-title'
-                : (this.$('.component-body').length > 0
-                ? '.component-body'
-                : null));
+    setupEventListeners: function() {},
 
-            if (!cssSelector) {
-                this.setCompletionStatus();
-            } else {
-                this.model.set('cssSelector', cssSelector);
-                this.$(cssSelector).on('inview', _.bind(this.inview, this));
-            }
-        },
+    checkIfResetOnRevisit: function() {
+      var isResetOnRevisit = this.model.get('_isResetOnRevisit');
 
+      // If reset is enabled set defaults
+      if (isResetOnRevisit) {
+        this.model.reset(isResetOnRevisit);
+      }
+    },
 
-        setupChart: function() {
-            var ctx = $("#myChart"+this.model.get('_id'));
-            var myChart = new Chart(ctx, {
-                type: this.model.get('_chartType'),
-                data: this.model.get('data'),
-                options: this.model.get('_options')
-            });
-
-            this.setReadyStatus();
-
-        },
-
-        setupEventListeners: function() {
-
-        },
-
-        checkIfResetOnRevisit: function() {
-            var isResetOnRevisit = this.model.get('_isResetOnRevisit');
-
-            // If reset is enabled set defaults
-            if (isResetOnRevisit) {
-                this.model.reset(isResetOnRevisit);
-            }
-        },
-
-        inview: function(event, visible, visiblePartX, visiblePartY) {
-            if (visible) {
-                if (visiblePartY === 'top') {
-                    this._isVisibleTop = true;
-                } else if (visiblePartY === 'bottom') {
-                    this._isVisibleBottom = true;
-                } else {
-                    this._isVisibleTop = true;
-                    this._isVisibleBottom = true;
-                }
-
-                if (this._isVisibleTop && this._isVisibleBottom) {
-                    this.$(this.model.get('cssSelector')).off('inview');
-                    this.setCompletionStatus();
-                }
-            }
-        },
-
-        remove: function() {
-            if ($("html").is(".ie8")) {
-                var obj = this.$("object")[0];
-                if (obj) {
-                    obj.style.display = "none";
-                }
-            }
-            ComponentView.prototype.remove.call(this);
-        },
-
-        onCompletion: function() {
-            this.setCompletionStatus();
-        },
-
-        onDeviceChanged: function() {
-
-        },
-
-        onScreenSizeChanged: function() {
-
-        },
-
-        onAccessibilityToggle: function() {
-
+    inview: function(event, visible, visiblePartX, visiblePartY) {
+      if (visible) {
+        if (visiblePartY === 'top') {
+          this._isVisibleTop = true;
+        } else if (visiblePartY === 'bottom') {
+          this._isVisibleBottom = true;
+        } else {
+          this._isVisibleTop = true;
+          this._isVisibleBottom = true;
         }
 
-    });
+        if (this._isVisibleTop && this._isVisibleBottom) {
+          this.$(this.model.get('cssSelector')).off('inview');
+          this.setCompletionStatus();
+        }
+      }
+    },
 
-		Adapt.register('chartjs', ChartJs);
-    return ChartJs;
+    remove: function() {
+      if ($('html').is('.ie8')) {
+        var obj = this.$('object')[0];
+        if (obj) {
+          obj.style.display = 'none';
+        }
+      }
+      this.$('.component-widget').off('inview');
+      ComponentView.prototype.remove.call(this);
+    },
 
+    onCompletion: function() {
+      this.setCompletionStatus();
+    },
+
+    onDeviceChanged: function() {},
+
+    onScreenSizeChanged: function() {},
+
+    onAccessibilityToggle: function() {},
+  });
+
+  Adapt.register('chartjs', ChartJs);
+  return ChartJs;
 });
